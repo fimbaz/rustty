f9cf500f3e779de16b7fea66badb2bdb8574a941
Cole Reynolds <cpjreynolds@gmail.com>
2015-10-10 14:49:30 +0000

    rebuild pages at 09da986

--- implementors/core/ops/trait.DerefMut.js
+++ implementors/core/ops/trait.DerefMut.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rustty'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.DerefMut.html' title='core::ops::DerefMut'>DerefMut</a> for <a class='struct' href='rustty/struct.Terminal.html' title='rustty::Terminal'>Terminal</a>",];
+implementors['rustty'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.DerefMut.html' title='core::ops::DerefMut'>DerefMut</a> for <a class='struct' href='rustty/struct.Terminal.html' title='rustty::Terminal'>Terminal</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
